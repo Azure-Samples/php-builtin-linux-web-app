c0d8d13ed2d2b570090505408dce3e923cda0500
Alex Chen <alexbiz@foxmail.com>
2017-08-24 11:29:23 +0800

    Auto-detect protocol for base_url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] =  "http://".$_SERVER['HTTP_HOST'];
+$config['base_url'] =  (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
 
 /*
 |--------------------------------------------------------------------------
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link rel="stylesheet" href="<?php echo base_url();?>/css/bootstrap.min.css">
   <script src="<?php echo base_url();?>/js/jquery.min.js"></script>
   <script src="<?php echo base_url();?>/js/bootstrap.min.js"></script>  
-  <script src="/js/modernizr-2.6.2.js"></script>
+  <script src="<?php echo base_url();?>/js/modernizr-2.6.2.js"></script>
   <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/css/jumbotron.css">
 </head>
 <body>
